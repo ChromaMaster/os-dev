c13210c79e17f177a6797576618d651aa9aebe7e
ChromaMaster <vlad.sabi18@gmail.com>
2019-06-22 13:38:12 +0200

    Add kernel_entry

---
+++ kernel/kernel_entry.asm
@@ -0,0 +1,6 @@
+; Ensures that we jump straight into the kernel’s entry function.
+[ bits 32]
+[ extern main ] ; Declate that we will be referencing the external symbol 'main',
+                ; so the linker can substitute the final address
+call main       ; invoke main () in our C kernel
+jmp $           ; Hang forever when we return from the kernel
